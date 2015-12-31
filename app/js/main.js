b8928f60237c652fa3a6430449139d266821178c
Maksfin <dyrnoj@bk.ru>
2015-12-31 12:35:05 +0300

    start project and add base template

---
+++ app/js/main.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+	
+	(function() {
+		var
+			toggleNav = $('.toggle-nav');
+			toggleNav.on('click', function() {
+				toggleNav.toggleClass('on');
+
+			})
+	})();
+
+	if($('.popup').length) {
+		Popup.init();
+	}
+	
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
+
+	$('.form').on('submit', function(e) {
+		e.preventDefault();
+	});
+
+	$('.slider').bxSlider({
+	 	// controls: false,
+	 	auto: true,
+	 	autoHover: true,
+	 	mode: 'fade',
+	 	speed: 800,
+ 	});	
+
+}); // --> end
+
+
+// Модуль 
+var Popup = (function() {
+	var
+		_popups = $('.popup');
+
+	var _close = function() {
+		_popups.hide();
+	};
+
+	return {
+		init: function() {
+			$('.').on('click', function(e) {
+				e.preventDefault();
+			});
+		}
+	};
+})();
